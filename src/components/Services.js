6edf3b8256a60cc3636b80c05b7008da5500349c
Mr-Mahabeer <mahabeer.fcs.gna@gmail.com>
2023-10-29 16:05:23 +0530

    fixed services section

--- src/components/Services.js
+++ src/components/Services.js
@@ -18,29 +18,30 @@ const Services = () => {
         <div className="row">
           <div className="col-sm-6 m-15px-tb">
             <div className="feature-box-01 media">
-              <i className="icon theme-bg icon-mobile" />
+              <i className="icon theme-bg icon-desktop" />
               <div className="feature-content media-body">
-                <h5>Web Design</h5>
+                <h5>Web Development
+                </h5>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  I am an experienced web developer with a strong passion for creating exceptional online experiences using cutting-edge technologies like React.js and Next.js. I specialize in crafting responsive and user-friendly websites and web applications. My expertise in React.js allows me to build dynamic and interactive user interfaces, while Next.js empowers me to create fast, server-rendered React applications.I thrive on tackling challenging projects and collaborating with cross-functional teams to bring creative web ideas to life. My goal is to continuously enhance and expand my skills in React.js and Next.js to provide the best possible web development services.
                 </p>
               </div>
             </div>
           </div>
           <div className="col-sm-6 m-15px-tb">
             <div className="feature-box-01 media">
-              <i className="icon theme-bg icon-desktop" />
+              <i className="icon theme-bg icon-mobile" />
               <div className="feature-content media-body">
-                <h5>Web Development</h5>
+                <h5>Mobile Development</h5>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  I possess significant experience as a mobile app developer, fueled by a strong passion for crafting exceptional mobile user experiences through React Native. My specialization lies in the creation of cross-platform mobile applications that seamlessly emulate the native feel on both iOS and Android platforms. Proficient in React Native, I excel in building dynamic and user-friendly mobile apps. My commitment to staying abreast of the latest trends in mobile app development drives me to consistently deliver high-quality solutions, characterized by clean and efficient code. My overarching objective is to continually elevate and broaden my proficiency in React Native.
+
+
                 </p>
               </div>
             </div>
           </div>
-          <div className="col-sm-6 m-15px-tb">
+          {/* <div className="col-sm-6 m-15px-tb">
             <div className="feature-box-01 media">
               <i className="icon theme-bg icon-target" />
               <div className="feature-content media-body">
@@ -51,46 +52,11 @@ const Services = () => {
                 </p>
               </div>
             </div>
-          </div>
-          <div className="col-sm-6 m-15px-tb">
-            <div className="feature-box-01 media">
-              <i className="icon theme-bg icon-mobile" />
-              <div className="feature-content media-body">
-                <h5>Web Design</h5>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-6 m-15px-tb">
-            <div className="feature-box-01 media">
-              <i className="icon theme-bg icon-desktop" />
-              <div className="feature-content media-body">
-                <h5>Web Development</h5>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-6 m-15px-tb">
-            <div className="feature-box-01 media">
-              <i className="icon theme-bg icon-target" />
-              <div className="feature-content media-body">
-                <h5>SEO Marketing</h5>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
-              </div>
-            </div>
-          </div>
+          </div> */}
+
         </div>
-        <div className="separated" />
-        <Testimonials />
+        {/* <div className="separated" /> */}
+        {/* <Testimonials /> */}
       </div>
     </section>
   );
